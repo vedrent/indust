73e87388cbcef5aa6416a34cee0029e5895ff05a
vedro <vedrentoff@gmail.com>
2023-02-25 16:35:26 +0300

    test

---
+++ first_practice/Triangle.h
@@ -0,0 +1,10 @@
+class Triangle {
+private:
+	double a, b, c;
+public:
+	bool extr_tr();
+	void set(double a1, double b1, double c1);
+	void show();
+	double perimetr();
+	double square();
+};
---
+++ first_practice/main.cpp
@@ -0,0 +1,7 @@
+﻿#include "Triangle.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
